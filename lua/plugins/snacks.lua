28e0f1c7a71f92447ef8d02f15b2927aa3ebb3a9
Gustav Eikaas <guei@equinor.com>
2025-11-18 21:07:29 +0100

    cleanup

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -1,9 +1,5 @@
 return {
   "folke/snacks.nvim",
-  opts = {
-     picker = {
-     }
-   },
    keys = {
     { "<leader><leader>", function() Snacks.picker.smart() end, desc = "Smart Find Files" },
     { "<leader>fw", function() Snacks.picker.grep() end, desc = "Grep" },
